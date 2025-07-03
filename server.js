72180ed03f8fc78972b9cbd310868920094b7d15
readyToRule <shivduttdwivedi099@gmail.com>
2025-07-04 01:05:15 +0530

    solving CORS Problem

--- server.js
+++ server.js
@@ -11,12 +11,20 @@ const dishRoutes = require('./routes/dishes');
 const app = express();
 
 const allowedOrigins = [
-  "http://localhost:3000",
-  "https://mern-food-client.vercel.app"
+  "https://mern-food-client.vercel.app",
+  "http://localhost:3000"
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps, curl, Postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
