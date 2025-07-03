a8f2126125656107e09b6cc2fbf62ffa48fc8c00
readyToRule <shivduttdwivedi099@gmail.com>
2025-07-04 01:16:38 +0530

    Adding debugger

--- server.js
+++ server.js
@@ -10,24 +10,7 @@ const dishRoutes = require('./routes/dishes');
 
 const app = express();
 
-const allowedOrigins = [
-  "https://mern-food-client.vercel.app",
-  "http://localhost:3000"
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps, curl, Postman)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
+app.use(cors());  // <--- debug line, allows ALL origins!
 
 app.use(express.json());
 
